e5fe8d8bcf80f7a2a34a8d657948be89c099d467
TechnicallyCoded <technicallycoded@gmail.com>
2023-11-27 15:04:08 +0100

    v1.0.0 - Initial commit

---
+++ src/main/java/com/tcoded/concretephysicsblocker/ConcretePhysicsBlocker.java
@@ -0,0 +1,17 @@
+package com.tcoded.concretephysicsblocker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ConcretePhysicsBlocker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
