7188e7953c99fc8fef344f435b8c5328d15d245c
TechnicallyCoded <technicallycoded@gmail.com>
2026-05-05 18:55:14 +0200

    First commit

--- src/main/java/com/tcoded/concretephysicsblocker/ConcretePhysicsBlocker.java
+++ src/main/java/com/tcoded/concretephysicsblocker/ConcretePhysicsBlocker.java
@@ -1,17 +1,60 @@
 package com.tcoded.concretephysicsblocker;
 
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFormEvent;
+import org.bukkit.event.block.BlockPhysicsEvent;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ConcretePhysicsBlocker extends JavaPlugin {
+import java.util.ArrayList;
+import java.util.List;
+
+public final class ConcretePhysicsBlocker extends JavaPlugin implements Listener {
+
+    private final List<Material> concreteBlocks = new ArrayList<>();
+
+    private boolean preventWaterTransform = true;
+    private boolean preventGravity = true;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // Config
+        saveDefaultConfig();
+        preventWaterTransform = getConfig().getBoolean("prevent-water-transform");
+        preventGravity = getConfig().getBoolean("prevent-gravity");
 
+        for (Material material : Material.values()) {
+            if (material.name().endsWith("CONCRETE_POWDER")) {
+                concreteBlocks.add(material);
+            }
+        }
+
+        getServer().getPluginManager().registerEvents(this, this);
+
+    }
+
+    @EventHandler
+    public void onConcretePhysics(EntityChangeBlockEvent event) {
+        if (!preventGravity) return;
+        if (!concreteBlocks.contains(event.getBlock().getType())) return;
+
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onConcreteHarden(BlockFormEvent event) {
+        if (!preventWaterTransform) return;
+        if (!concreteBlocks.contains(event.getBlock().getType())) return;
+
+        event.setCancelled(true);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        HandlerList.unregisterAll(((Plugin) this));
     }
 }
